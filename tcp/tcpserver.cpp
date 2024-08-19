57cc0c89e190e8e2f30d31fc7055549a5a573704
Don Silvio <svobodacom1@gmail.com>
2024-08-19 14:06:55 +0300

    finished all methods in tcpserver.cpp

--- tcp/tcpserver.cpp
+++ tcp/tcpserver.cpp
@@ -3,7 +3,7 @@
 TcpServer::TcpServer(QObject *parent)
     : QTcpServer(parent)
 {
-    qDebug() << this << "created";
+    qDebug() << this << "created on " << QThread::currentThread();
 }
 
 TcpServer::~TcpServer()
@@ -27,39 +27,72 @@ bool TcpServer::listen(const QHostAddress &address, quint16 port)
 
     m_connections->moveToThread(m_thread);
     m_thread->start();
+
+    return true;
 }
 
 
 
 void TcpServer::close()
 {
-
+    qDebug() << this << "closing server";
+    emit finished();
+    QTcpServer::close();
 }
 
 
 
 qint64 TcpServer::port()
 {
-
+    if(!isListening())
+    {
+        return this->serverPort();
+    }
+    else
+    {
+        return 1000;
+    }
 }
 
 
 
 void TcpServer::incomingConnection(qintptr descriptor)
 {
-
+    qDebug() << this << "attemting to accept connection" << descriptor;
+    TcpConnection *connection = new TcpConnection();
+    accept(descriptor, connection);
 }
 
 
 
 void TcpServer::accept(qintptr descriptor, TcpConnection *connection)
 {
-
+    qDebug() << this << "accepting the connection" << descriptor;
+    connection->moveToThread(m_thread);
+    emit accepting(descriptor, connection);
 }
 
 
 
 void TcpServer::complete()
 {
+    if(!m_thread)
+    {
+        qWarning() << this << "exiting complete there was no thread!";
+        return;
+    }
 
+    qDebug() << this << "Complete called, destroying thread";
+    delete m_connections;
+
+    qDebug() << this << "Quitting thread";
+    m_thread->quit();
+    m_thread->wait();
+
+    delete m_thread;
+
+    qDebug() << this << "complete";
 }
+
+
+
