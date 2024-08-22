63e801de671a427e7cacfc330f49fef0785308e1
Don Silvio <svobodacom1@gmail.com>
2024-08-22 15:19:41 +0300

    making httpserver.cpp

--- http/httpserver.cpp
+++ http/httpserver.cpp
@@ -3,38 +3,54 @@
 
 HttpServer::HttpServer(QObject *parent) : TcpServer(parent)
 {
-
+    m_root = QCoreApplication::applicationDirPath();
+    m_rate = 15360;
 }
 
-HttpServer::~HttpServer()
-{
-
-}
+HttpServer::~HttpServer() { }
 
 
 
 QString HttpServer::root()
 {
-
+    return m_root;
 }
 
 
 
 void HttpServer::setRoot(QString path)
 {
+    m_root = path;
+    if (m_root.endsWith("/") || m_root.endsWith("\\"))
+    {
+        m_root = m_root.mid(0,m_root.length() - 1);
+    }
+    qDebug() << this << "root set to: " << m_root;
+}
 
+
+
+int HttpServer::rate()
+{
+     return m_rate;
 }
 
 
 
 void HttpServer::setRate(int value)
 {
-
+    m_rate = value;
+    qDebug() << this << "rate set to: " << value;
 }
 
 
 
 void HttpServer::incomingConnection(qintptr descriptor)
 {
+    qDebug() << this << "Incoiming HTTP connection: " << descriptor;
 
+    HttpConnection *connection = new HttpConnection();
+
+    // TO DO = set rate and root
+    accept(descriptor, connection);
 }
